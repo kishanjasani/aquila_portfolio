a2846cf530bc3841efbc9b3205e9ba9f50ff016a
Kishan Jasani <kishanjasani007@yahoo.in>
2020-11-24 18:30:26 +0530

    Add basic theme files

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main template file.
+ *
+ * @package Aquila_Portfolio
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Aquila Portfolio</title>
+</head>
+<body>
+	<h1>Hello WordPress</h1>
+</body>
+</html>
