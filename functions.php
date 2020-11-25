ad8ddf780b3556b17aab5b5552eedabb4f6ec217
Kishan Jasani <kishanjasani007@yahoo.in>
2020-11-25 14:13:53 +0530

    Enqueue stylesheet and add wp_body_open support

--- functions.php
+++ functions.php
@@ -4,3 +4,9 @@
  *
  * @package Aquila_Portfolio
  */
+
+function aquila_portfolio_enqueue_script() {
+	wp_enqueue_style( 'stylesheet', get_template_directory_uri() . '/style.css', [], filemtime( get_template_directory() . '/style.css' ), 'all' );
+}
+
+add_action( 'wp_enqueue_scripts', 'aquila_portfolio_enqueue_script' );
