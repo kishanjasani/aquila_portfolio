1616c1210a62835dac5efd7ec538abb21ebfe4f4
Kishan Jasani <kishanjasani007@yahoo.in>
2020-11-25 14:36:40 +0530

    Add phpcs.xml and enqueue scripts

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@
 
 function aquila_portfolio_enqueue_script() {
 	wp_enqueue_style( 'stylesheet', get_template_directory_uri() . '/style.css', [], filemtime( get_template_directory() . '/style.css' ), 'all' );
+	wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime( get_template_directory() . '/assets/main.js' ), true );
 }
 
 add_action( 'wp_enqueue_scripts', 'aquila_portfolio_enqueue_script' );
